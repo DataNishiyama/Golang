9d16a2baf0b438a8328aeb4036addfd4c57298e6
Anthony Nishiyama <datanishiyama@gmail.com>
2021-12-31 14:07:51 +0900

    Created first lines in Go (Hello World) app

---
+++ wtg_01.go
@@ -0,0 +1,24 @@
+/*
+DataNishiyama (MultiLine Comment Block)
+Educative : Way to Go : Overview of Functions
+Date      : 12/31/2021
+Purpose   : Writing first programs in Go (VSCode)
+*/
+
+// make package standalone executable
+package main
+
+// import package
+import "fmt"
+
+// entry point via main function
+func main() {
+
+	// print using fmt func
+	fmt.Println("Hello World")
+
+	// Mulilanguage Support
+	fmt.Println("こんにちは アンソニ")
+
+	// exit program
+}
