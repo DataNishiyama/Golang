c628ca60a479a8dc35e771fcf785c0eb70cd462f
Anthony Nishiyama <datanishiyama@gmail.com>
2022-01-04 19:56:55 +0900

    Create wtg_02.go
    Values (go by example)

---
+++ wtg_02.go
@@ -0,0 +1,19 @@
+// package
+package main
+
+// imports
+import "fmt"
+
+// entry point
+func main() {
+
+	// Print line combination
+	fmt.Println("go" + "lang")
+
+	// math vs print
+	fmt.Println("1+1", 1+1)
+
+	// bool
+	fmt.Println(true || false)
+
+}
