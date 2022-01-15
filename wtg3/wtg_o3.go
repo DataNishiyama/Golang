a1ca008d64a2646d89994cfc5e7d2ab5b5746766
Anthony Nishiyama <datanishiyama@gmail.com>
2022-01-15 19:50:05 +0900

    Rename and Restructure:
    Way to go
    Added Lesson 3: Type-casting in Golang

---
+++ wtg3/wtg_o3.go
@@ -0,0 +1,22 @@
+//Type casting in Golang
+
+//Package main
+package main
+
+//Import fmt
+import "fmt"
+
+//func main
+func main() {
+
+	//Declare float point variable
+	var number float32 = 5.2
+
+	//Print value of variable
+	fmt.Println(number)
+
+	//Print type cast result
+	fmt.Println(int(number))
+
+	// end Main
+}
